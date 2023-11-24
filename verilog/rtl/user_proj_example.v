84ac0746c424aa422207946bc66454a254f3bfb1
Kiran Reddy <89727621+KKiranR@users.noreply.github.com>
2023-11-24 23:42:13 +0530

    updated

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -35,9 +35,7 @@
  *-------------------------------------------------------------
  */
 
-module user_proj_example #(
-    parameter BITS = 16
-)(
+module user_proj_example (
 `ifdef USE_POWER_PINS
     inout vccd1,	// User area 1 1.8V supply
     inout vssd1,	// User area 1 digital ground
@@ -45,112 +43,82 @@ module user_proj_example #(
 
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
-    input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
+    //input wb_rst_i,
+ //   input wbs_stb_i,
+   // input wbs_cyc_i,
+   // input wbs_we_i,
+//    input [3:0] wbs_sel_i,
+ //   input [31:0] wbs_dat_i,
+//    input [31:0] wbs_adr_i,
+  //  output wbs_ack_o,
+ //   output [31:0] wbs_dat_o,
 
     // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
+//    input  [127:0] la_data_in,
+ //   output [127:0] la_data_out,
+ //   input  [127:0] la_oenb,
 
     // IOs
-    input  [BITS-1:0] io_in,
-    output [BITS-1:0] io_out,
-    output [BITS-1:0] io_oeb,
+    input  [22:0] io_in,
+    output [11:0] io_out
+ //   output [BITS-1:0] io_oeb,
 
     // IRQ
-    output [2:0] irq
+   // output [2:0] irq
 );
-    wire clk;
-    wire rst;
+    
 
-    wire [BITS-1:0] rdata; 
-    wire [BITS-1:0] wdata;
-    wire [BITS-1:0] count;
+wire [10:0]product;
+wire done;
 
-    wire valid;
-    wire [3:0] wstrb;
-    wire [BITS-1:0] la_write;
-
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = {{(32-BITS){1'b0}}, rdata};
-    assign wdata = wbs_dat_i[BITS-1:0];
-
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(BITS){rst}};
-
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
-    // LA
-    assign la_data_out = {{(128-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:64-BITS] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i[BITS-1:0]),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:64-BITS]),
-        .count(count)
-    );
-
+pes_fpmul u1(.clk(wb_clk_i),.in_ready(io_in[22]),.a(io_in[21:11]),.b(io_in[10:0]),.product(io_out[11:1]),.done(io_out[0]));
+assign io_out={product,done};
 endmodule
-
-module counter #(
-    parameter BITS = 16
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output reg ready,
-    output reg [BITS-1:0] rdata,
-    output reg [BITS-1:0] count
+module pes_fpmul(
+input clk,in_ready,
+input [10:0] a,b,
+output  reg [10:0]product,
+output reg done
 );
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 1'b0;
-            ready <= 1'b0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1'b1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
-    end
+reg sign_a,sign_b,sign_p;
+reg [5:0] exp_a,exp_b,exp_p;
+reg [3:0] man_a,man_b,man_p;
+reg [5:0] temp_expa,temp_expb,temp_expp;
+reg [4:0] temp_mana,temp_manb;
+reg [9:0]temp_manp;
 
+always@(posedge clk) begin
+if(a==0||b==0)begin
+product=0;
+done=1'b1;
+end
+else begin
+if(in_ready)begin
+{sign_a,exp_a,man_a}<=a;
+{sign_b,exp_b,man_b}<=b;
+temp_mana<={1'b1,man_a};
+temp_manb<={1'b1,man_b};
+temp_expa<=exp_a-6'd31;
+temp_expb<=exp_b-6'd31;
+
+sign_p<=sign_a^sign_b;
+temp_expp <= temp_expa+temp_expb;
+temp_manp <= temp_mana*temp_manb;
+if(temp_manp[9]) begin
+man_p<=temp_manp[8:5];
+exp_p<=temp_expp+6'd32;
+product<={sign_p,exp_p,man_p};
+done<=1'b1;
+end
+else if(temp_manp[9]==0)begin
+man_p<=temp_manp[7:4];
+exp_p<=temp_expp+6'd31;
+product<={sign_p,exp_p,man_p};
+done<=1'b1;
+end
+end
+end
+end
 endmodule
 `default_nettype wire
